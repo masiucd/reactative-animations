000190beff42b2f9c47e7928e9cfe4eb611a94bb
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-01-29 20:01:29 +0100

    (feat): move array helpers

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,1 +1,18 @@
 export const randomNumber = (num = 10) => Math.floor(Math.random() * num)
+
+export const arrayMoveMutate = <T>(xs: T[], from: number, to: number) => {
+  const startIndex = from < 0 ? xs.length + from : from
+  if (startIndex >= 0 && startIndex < xs.length) {
+    const endIndex = to < 0 ? xs.length + to : to
+    const [x] = xs.splice(from, 1)
+    xs.splice(endIndex, 0, x)
+  }
+}
+
+const arrayMove = <T>(xs: T[], from: number, to: number) => {
+  const list = [...xs]
+  arrayMoveMutate(list, from, to)
+  return list
+}
+
+export { arrayMove, arrayMoveMutate }
